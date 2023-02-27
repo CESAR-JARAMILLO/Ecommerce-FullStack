6b35781d996cd61d07a721b037b5258ae6f56f25
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-02-27 11:31:40 -0800

    added register user authentication

--- backend/base/views.py
+++ backend/base/views.py
@@ -8,10 +8,10 @@ from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
-
-# Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
   def validate(self, attrs):
@@ -26,14 +26,23 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
   serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getRoutes(request):
-  routes = [
-    "/api/products/",
-    "/api/products/create/",
-    "/api/products/upload/",
-  ]
-  return Response(routes)
+@api_view(['POST'])
+def registerUser(request):
+  data = request.data
+
+  try:
+    user = User.objects.create(
+      first_name = data['name'],
+      username = data['email'],
+      email = data['email'],
+      password = make_password(data['password'])
+    )
+
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+  except:
+    message = {'detail':'User with this email already exists'}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
